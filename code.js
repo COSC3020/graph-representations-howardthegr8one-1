6091037c1d94f73ce48a675a6f26b9e80f4bccd0
Howard Shaw <68131536+howardthegr8one@users.noreply.github.com>
2024-03-22 15:42:05 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,23 @@
 function convertToAdjList(adjMatrix) {
-    return adjMatrix;
+    let adjList = [];
+    for (let i = 0; i < adjMatrix.length; i++) {
+        let edges = [];
+        for (let j = 0; j < adjMatrix.length; j++) {
+            if (adjMatrix[i][j] == 1) edges.push(j)
+        }
+        adjList.push(edges)
+    }
+    return adjList
 }
+
+function convertToAdjMatrix(adjList) {
+    let adjMatrix = [];
+    for (let i = 0; i < adjList.length; i++) {
+        let row = new Array(adjList.length).fill(0)
+        for (nodes in adjList[i]) {
+            row[adjList[i][nodes]] = 1
+        }
+        adjMatrix[i] = row
+    }
+    return adjMatrix
+}   
